87e52c50d83944d5012ffa6078ed8b51c2724c06
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-10-30 14:37:29 +0100

    add db path

---
+++ src/contacts.js
@@ -0,0 +1,9 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath);
